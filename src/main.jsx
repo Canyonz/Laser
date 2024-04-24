2c7564d5032f3cc8f02ac850c3a0824e03054ece
Canyonz <Faton64rus@yandex.ru>
2024-04-24 23:21:22 +0300

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,25 @@ import Home from "./components/screen/home/Home";
 import "./assets/styles/global.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Home />
+		</BrowserRouter>
+	</React.StrictMode>
 );
+
+/*
+{
+  "schemaVersion": 2,
+  "dockerfileLines": [
+    "FROM node:20.11.0",
+    "WORKDIR /app",
+    "COPY package.json yarn.lock ./",
+    "RUN yarn install",
+    "COPY . .",
+    "RUN yarn build",
+    "EXPOSE 80",
+    "CMD [\"yarn\", \"preview\", \"--port\", \"80\"]"
+  ]
+}
+*/
